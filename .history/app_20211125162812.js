d04a5fd18ab696f3e0dc3719fc4270a0433f0171
helensroberts79@gmail.com <helensroberts79@gmail.com>
2022-01-05 16:08:37 +0000

    first commit

---
+++ .history/app_20211125162812.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.getElementById('score');
+    const width = 28; //28 x 28 = 784 squares
+
+    //layout of grid and what is in the squares
+
+    const layout
+});
---
+++ .history/app_20211125162836.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid');
+    const scoreDisplay = document.getElementById('score');
+    const width = 28; //28 x 28 = 784 squares
+
+    //layout of grid and what is in the squares
+
+    const layout = [
+        1,1,
+    ]
+});
